0805f1d9d3fbe389686267bf03c59eb34efad4af
Hendra Saputra <hendrasaputra@hendrasaputra.local>
2019-01-08 11:54:29 +0700

    3 commit

---
+++ else.py
@@ -0,0 +1,24 @@
+hari_ini = "Minggu"
+
+if(hari_ini == "Senin"):
+    print("Saya akan kuliah")
+
+elif(hari_ini == "Selasa"):
+    print("Saya akan kuliah")
+
+elif(hari_ini == "Rabu"):
+    print("Saya akan kuliah")
+
+elif(hari_ini == "Kamis"):
+    print("Saya akan kuliah")
+
+elif(hari_ini == "Jumat"):
+    print("Saya akan kuliah")
+
+elif(hari_ini == "Sabtu"):
+    print("Saya akan kuliah")
+
+elif(hari_ini == "Minggu"):
+    print("Saya akan libur")
+
+#hasil diatas jika dijalankan , maka result nya adalah 'saya akan libur'
